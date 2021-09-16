6e331fb6f99a250c8a0225f547796841ee5b53d7
Cory Freres <cfreres@hotmail.com>
2021-09-16 15:04:42 -0500

    Add project files.

---
+++ AddressBook/MainPage.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using DataAccessLibrary;
+using System.Data;
+using System.Collections.ObjectModel;
+
+namespace AddressBook
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            //On startup, show initialized database
+            var collection = new ObservableCollection<Entry>();
+            foreach (DataRow row in DataAccess.GetData().Rows)
+            {
+                var obj = new Entry()
+                {
+                    Name = (string)row.ItemArray[0],
+                    Date = (string)row.ItemArray[1],
+                    PhoneNumber = (string)row.ItemArray[2],
+                    Address = (string)row.ItemArray[3]
+                };
+                collection.Add(obj);
+            }
+            dataGrid.DataContext = collection;
+        }
+
+        //On search button click check textboxes for text to determine which search queries to run
+        //then update and show query results
+        private void Search(object sender, RoutedEventArgs e)
+        {
+            DataTable dt = new DataTable();
+            if (NameSearch.Text.Length > 0)
+            {
+                if (lowDateSearch.Text.Length > 0 && highDateSearch.Text.Length > 0)
+                {
+                    dt = DataAccess.GetDateAndNameSearch(lowDateSearch.Text, highDateSearch.Text, NameSearch.Text);
+                }
+                else
+                {
+                    dt = DataAccess.GetNameSearch(NameSearch.Text);
+                }
+            }
+            else if(lowDateSearch.Text.Length > 0 && highDateSearch.Text.Length > 0)
+            {
+                dt = DataAccess.GetDateSearch(lowDateSearch.Text, highDateSearch.Text);
+            }
+            else
+            {
+                dt = DataAccess.GetData();
+            }
+
+            var collection = new ObservableCollection<Entry>();
+            foreach(DataRow row in dt.Rows)
+            {
+                var obj = new Entry()
+                {
+                    Name = (string)row.ItemArray[0],
+                    Date = (string)row.ItemArray[1],
+                    PhoneNumber = (string)row.ItemArray[2],
+                    Address = (string)row.ItemArray[3]
+                };
+                collection.Add(obj);
+            }
+
+            dataGrid.DataContext = collection;
+        }
+
+        //invoke insert query if all boxes contain text
+        private void Add(object sender, RoutedEventArgs e)
+        {
+            if (Name.Text.Length > 0 && BirthDate.Text.Length > 0 && PhoneNumber.Text.Length > 0 && Address.Text.Length > 0)
+            {
+                DataAccess.AddData(Name.Text, BirthDate.Text, PhoneNumber.Text, Address.Text);
+            }
+        }
+
+        //invoke delete query by primary key name
+        private void Delete(object sender, RoutedEventArgs e)
+        {
+            if (Name.Text.Length > 0)
+            {
+                DataAccess.DeleteData(Name.Text);
+            }
+        }
+    }
+
+    //Class to store address book data
+    public class Entry
+    {
+        public string Name { get; set; }
+        public string Date { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        
+    }
+}
---
+++ DataAccessLibrary/DataAccess.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using Windows.Storage;
+
+namespace DataAccessLibrary
+{
+    public static class DataAccess
+    {
+        //Initialize empty database with proper address book columns
+        public async static void InitializeDatabase()
+        {
+            await ApplicationData.Current.LocalFolder.CreateFileAsync("addressBook.db", CreationCollisionOption.OpenIfExists);
+            string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "addressBook.db");
+            using (SqliteConnection db =
+               new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                String dropTableCommand = "DROP TABLE IF EXISTS AddressBook;";
+                String tableCommand = "CREATE TABLE " +
+                    "AddressBook (Name TEXT PRIMARY KEY, BirthDate DATE, PhoneNumber TEXT, Address TEXT);";
+
+                SqliteCommand dropTable = new SqliteCommand(dropTableCommand, db);
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+                
+                dropTable.ExecuteReader();
+                createTable.ExecuteReader();
+
+                db.Close();
+            }
+            
+        }
+
+        //Handle insert query
+        public static void AddData(string nameText, string dateText, string phoneText, string addressText)
+        {
+            string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "addressBook.db");
+            using (SqliteConnection db =
+              new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+                // Use parameterized query to prevent SQL injection attacks
+                insertCommand.CommandText = "INSERT INTO AddressBook VALUES (@Name, @Date, @Phone, @Address);";
+                insertCommand.Parameters.AddWithValue("Name", nameText);
+                insertCommand.Parameters.AddWithValue("@Date", dateText);
+                insertCommand.Parameters.AddWithValue("@Phone", phoneText);
+                insertCommand.Parameters.AddWithValue("@Address", addressText);
+
+                insertCommand.ExecuteReader();
+
+                db.Close();
+            }
+        }
+
+        //Handle delete query
+        public static void DeleteData(string nameText)
+        {
+            string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "addressBook.db");
+            using (SqliteConnection db =
+              new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                SqliteCommand deleteCommand = new SqliteCommand();
+                deleteCommand.Connection = db;
+
+                // Use parameterized query to prevent SQL injection attacks
+                deleteCommand.CommandText = "DELETE FROM AddressBook WHERE Name = @Name";
+                deleteCommand.Parameters.AddWithValue("@Name", nameText);
+                deleteCommand.ExecuteReader();
+
+                db.Close();
+            }
+        }
+
+        //Handle select statement for the entire database
+        public static DataTable GetData()
+        {
+            DataTable addressBook = new DataTable();
+            
+            string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "addressBook.db");
+            using (SqliteConnection db =
+               new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                SqliteCommand selectCommand = new SqliteCommand
+                    ("SELECT * FROM AddressBook", db);
+
+                SqliteDataReader query = selectCommand.ExecuteReader();
+
+                addressBook.Load(query);
+
+                
+
+                db.Close();
+            }
+
+            return addressBook;
+        }
+
+        //Handle select query for specific name
+        public static DataTable GetNameSearch(string name)
+        {
+            DataTable addressBook = new DataTable();
+
+            string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "addressBook.db");
+            using (SqliteConnection db =
+               new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                SqliteCommand selectCommand = new SqliteCommand
+                    ("SELECT * FROM AddressBook WHERE Name = @name", db);
+                selectCommand.Parameters.AddWithValue("@name", name);
+
+                SqliteDataReader query = selectCommand.ExecuteReader();
+
+                addressBook.Load(query);
+
+                db.Close();
+            }
+
+            return addressBook;
+        }
+
+        //Handle select query for between two dates
+        public static DataTable GetDateSearch(string lowerBound, string upperBound)
+        {
+            DataTable addressBook = new DataTable();
+
+            string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "addressBook.db");
+            using (SqliteConnection db =
+               new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                SqliteCommand selectCommand = new SqliteCommand
+                    ("SELECT * FROM AddressBook WHERE BirthDate BETWEEN @lower and @upper", db);
+                selectCommand.Parameters.AddWithValue("@lower", lowerBound);
+                selectCommand.Parameters.AddWithValue("@upper", upperBound);
+
+                SqliteDataReader query = selectCommand.ExecuteReader();
+
+                addressBook.Load(query);
+
+                db.Close();
+            }
+
+            return addressBook;
+        }
+
+        //Handle select query for date and name search combined
+        public static DataTable GetDateAndNameSearch(string lowerBound, string upperBound, string name)
+        {
+            DataTable addressBook = new DataTable();
+
+            string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "addressBook.db");
+            using (SqliteConnection db =
+               new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                SqliteCommand selectCommand = new SqliteCommand
+                    ("SELECT * FROM AddressBook WHERE (BirthDate BETWEEN @lower and @upper) AND (Name = @name)", db);
+                selectCommand.Parameters.AddWithValue("@lower", lowerBound);
+                selectCommand.Parameters.AddWithValue("@upper", upperBound);
+                selectCommand.Parameters.AddWithValue("@name", name);
+
+                SqliteDataReader query = selectCommand.ExecuteReader();
+
+                addressBook.Load(query);
+
+                db.Close();
+            }
+
+            return addressBook;
+        }
+    }
+}
